eb7584afccf39421c2f1e30db0095382b9e61b8c
Tymur <neznaya941@gmail.com>
2026-01-31 17:10:19 +0200

    Delete urok2.py

--- urok2.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Student: 
-    def __init__(self,name):
-        self.name = name 
-        self.gladness = 50 
-        self.progress = 0
-        self.alive = True 
-        
