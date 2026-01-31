516e89ba522cde9f3f3bbc1b326225bf3748004d
Tymur <neznaya941@gmail.com>
2026-01-31 16:59:01 +0200

    Add files via upload

---
+++ urok2.py
@@ -0,0 +1,9 @@
+import random
+
+class Student: 
+    def __init__(self,name):
+        self.name = name 
+        self.gladness = 50 
+        self.progress = 0
+        self.alive = True 
+        
