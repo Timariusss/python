a1da67f5cba002a748f556537219b45c051d15fd
Timariusss <neznaya941@gmail.com>
2026-02-27 09:01:05 +0200

    velvetine

---
+++ dz7.py
@@ -0,0 +1,17 @@
+class NumberCollection:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        return self._generator()
+
+    def _generator(self):
+        current = self.start
+        while current <= self.end:
+            yield current
+            current += 1
+
+numbers = NumberCollection(1, 5)
+for num in numbers:
+    print(num)
